0ffe1a84b0414d179536133fc1e8e3138d10b8d3
rsensenig <r.a.sensenig@gmail.com>
2022-10-30 10:26:38 -0400

    created server.js and created mongodb connection

---
+++ server/server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const router = express.Router();
+require("dotenv").config({ path: "./config.env" });
+const port = process.env.PORT || 5000;
+
+require('./db/connection');
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
